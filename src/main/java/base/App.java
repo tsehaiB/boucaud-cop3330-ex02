76f0b02e71608559489836a1b0866c1d752234bf
Art3mis <tsehaib@knights.ucf.edu>
2021-06-05 15:57:38 -0400

    POM update, OOP Solution for Exercise 2

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -1,4 +1,31 @@
 package base;
 import java.util.Scanner;
 public class App {
+    static Scanner inp = new Scanner(System.in);
+
+    public static void main(){
+        App myApp = new App();
+
+        String name = myApp.readIn();
+        int size = myApp.findSize(name);
+        myApp.concatenate(size, name);
+    }
+
+    public String readIn() {
+        System.out.print("What is the input string? ");
+        String str = inp.nextLine();
+        while (str.equals(""))
+        {
+            System.out.print("Please input a valid string.");
+            str = inp.nextLine();
+        }
+        return str;
+    }
+    public int findSize(String str){
+        return str.length();
+    }
+    public void concatenate(int l, String s) {
+        String out = s + " has " + l + " characters.";
+        System.out.println(out);
+    }
 }
